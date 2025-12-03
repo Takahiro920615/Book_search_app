bbfc2bd0079e21cc500adf39971e836072f58ba3
shima615 <deaman6948@gmail.com>
2025-12-04 06:08:51 +0900

    application.rbフロントからのリクエスト許可用のコード追加

--- viteproject-backend/config/application.rb
+++ viteproject-backend/config/application.rb
@@ -10,6 +10,22 @@ module ViteprojectBackend
     config.load_defaults 7.2
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
+
+    # フロントのVercelからのリクエストを許可する
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://book-search-nkwijwnqc-takahiro-shimatanis-projects.vercel.app',
+                'localhost:3000',
+                '127.0.0.1:3000'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          expose: ['Authorization'],
+          credentials: true  # クッキー送信に絶対必要！
+      end
+    end
+
     # ブラウザとサーバー間でクッキーを扱うためのミドルウェア
     config.middleware.use ActionDispatch::Cookies
     # クッキーベースのセッション管理を提供するミドルウェア
