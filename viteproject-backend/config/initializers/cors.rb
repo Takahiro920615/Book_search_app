98877ece7c80cb5dfe54522c31cf409716ba5f5d
shima615 <deaman6948@gmail.com>
2026-01-13 22:36:39 +0900

    cors.rb修正

--- viteproject-backend/config/initializers/cors.rb
+++ viteproject-backend/config/initializers/cors.rb
@@ -1,9 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://book-search-5zy90wiol-takahiro-shimatanis-projects.vercel.app", "https://*.vercel.app"
+   
+    origins "https://book-search-app-pearl.vercel.app"
+
+    # 必要に応じてローカル開発用も追加（テスト時用）
+    # origins "https://book-search-app-pearl.vercel.app", "http://localhost:5173"
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true   # cookie/JWT用
+      credentials: true,          # withCredentials: true を許可（クッキー/JWT用）
+      expose: ['Authorization']   # Authorizationヘッダーを露出（任意だがおすすめ）
   end
 end
