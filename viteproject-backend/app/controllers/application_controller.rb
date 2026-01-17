071866401d82eaafbc157d4e7528b79055b83d87
shima615 <deaman6948@gmail.com>
2026-01-17 22:37:01 +0900

    application_controller.rb編集

--- viteproject-backend/app/controllers/application_controller.rb
+++ viteproject-backend/app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
 
   protect_from_forgery with: :null_session # CSRFトークンがなくても破棄しない
+  
+  rescue_from StandardError do |e|
+    Rails.logger.error "500 ERROR: #{e.class} - #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+  
+    render json: {
+      error: e.message,
+      type: e.class.name,
+      backtrace: e.backtrace&.first(5)
+    }, status: :internal_server_error
+  end
+
 end
