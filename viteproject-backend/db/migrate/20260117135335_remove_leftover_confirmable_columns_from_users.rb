8c0f76bbb386b2addbd91b3bbda55ae758bb25f7
shima615 <deaman6948@gmail.com>
2026-01-21 22:43:47 +0900

    編集

--- viteproject-backend/db/migrate/20260117135335_remove_leftover_confirmable_columns_from_users.rb
+++ viteproject-backend/db/migrate/20260117135335_remove_leftover_confirmable_columns_from_users.rb
@@ -4,7 +4,6 @@ class RemoveLeftoverConfirmableColumnsFromUsers < ActiveRecord::Migration[7.2]
     remove_column :users, :confirmed_at, :datetime         if column_exists?(:users, :confirmed_at)
     remove_column :users, :confirmation_sent_at, :datetime if column_exists?(:users, :confirmation_sent_at)
     remove_column :users, :unconfirmed_email, :string      if column_exists?(:users, :unconfirmed_email)
-
     # remove_index :users, "index_users_on_confirmation_token" if index_exists?(:users, "index_users_on_confirmation_token")
   end
 end
