f82d57f4cb2bf16c109dd99b41737f2e3b22a6f7
shima615 <deaman6948@gmail.com>
2026-02-20 18:06:46 +0900

    routes.rb修正

--- viteproject-backend/config/routes.rb
+++ viteproject-backend/config/routes.rb
@@ -12,15 +12,15 @@ Rails.application.routes.draw do
 
   get '/test', to: 'test#index'
 
-  # namespace :api, defaults: { format: :json } do
-  #   namespace :v1 do
-  #     resources :favorites, only: [:index, :create, :destroy], param: :book_id
-  #   end
+  # # namespace :api, defaults: { format: :json } do
+  # #   namespace :v1 do
+  # #     resources :favorites, only: [:index, :create, :destroy], param: :book_id
+  # #   end
 
-    # v1の外でも使えるように互換ルート（フロントが /api/user で動くように）
-    get 'user', to: 'users#show'
-    get 'protected', to: 'users#protected'
-  end
+  #   # v1の外でも使えるように互換ルート（フロントが /api/user で動くように）
+  #   get 'user', to: 'users#show'
+  #   get 'protected', to: 'users#protected'
+  # end
 
   root to: proc { [200, {}, [{status: "API is running"}.to_json]] }
 end
